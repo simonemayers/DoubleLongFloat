0d2d5b76740fbf2d4a75f6631de6b342522a31a4
Simone Mayers <53239742+simonemayers@users.noreply.github.com>
2022-05-03 15:46:12 -0400

    done

---
+++ src/DoubleLongFloat.java
@@ -0,0 +1,14 @@
+public class DoubleLongFloat {
+    public static void main(String[] args){
+        int a = -9;
+        long b = 9999999999L;
+        float c = 2.5F;
+        double d = 5.4;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
